71923dd950d56afcea67db45f9c43125eaa58d32
Brian <b.perez3237@gmail.com>
2022-01-26 20:33:18 -0500

    new file

---
+++ section6.js
@@ -0,0 +1,10 @@
+const p = document.createElement("p");
+p.textContent = "This content added by JavaScript";
+
+document.querySelector("#section-6").appendChild(p);
+
+
+function click1() {
+    const box = document.getElementById("section-6");
+    box.getElementById("section-6")
+}
